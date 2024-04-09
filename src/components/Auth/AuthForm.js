5db753cb9abc00231eb12d78a9e3329f159e7702
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-04-09 23:18:52 +0530

    useContext

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,13 @@
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
 
 import classes from "./AuthForm.module.css";
+import AuthContext from "../../store/auth-context";
 
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -54,9 +57,8 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
+        authCtx.login(data.idToken);
+      }).catch((err) => {
         alert(err.Message);
       });
   };
