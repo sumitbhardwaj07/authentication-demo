e03577c29837669aa94e8702e73c24c0ae5de1e3
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-04-10 00:41:05 +0530

    stay login after refresh

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import { useContext } from "react";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <Layout>
       <Routes>
-      <Route path='/' element={<HomePage />} exact />
-        <Route path='/auth' element={<AuthPage />} />
-        <Route path='/profile' element={<UserProfile />} />
+        <Route path="/" element={<HomePage />} exact />
+        {!authCtx.isLoggedIn && (<Route path="/auth" element={<AuthPage />} />)}
+        {authCtx.isLoggedIn && <Route path="/profile" element={<UserProfile />} />}
+        <Route path="*" element={<Navigate replace to="/"/>} />
       </Routes>
     </Layout>
   );
