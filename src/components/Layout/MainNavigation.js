cd2a729fcdb2dfd32f1e7c5d0fee4a4dd554119e
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-04-10 11:03:46 +0530

    auto logout

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,16 +1,42 @@
 import { Link } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import AuthContext from "../../store/auth-context";
 
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
+  const [logoutTimer, setLogoutTimer] = useState(null);
 
   const isLoggedIn = authCtx.isLoggedIn;
-   const logoutHandler = () =>{
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const remainingTime = authCtx.tokenExpiration - Date.now();
+      setLogoutTimer(
+        setTimeout(() => {
+          authCtx.logout();
+        }, remainingTime)
+      );
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [isLoggedIn, authCtx.tokenExpiration, authCtx.logout]);
+
+  const logoutHandler = () => {
+    clearTimeout(logoutTimer);
     authCtx.logout();
-   };
+  };
+
+  const resetLogoutTimer = () => {
+    clearTimeout(logoutTimer);
+    const remainingTime = authCtx.tokenExpiration - Date.now();
+    setLogoutTimer(
+      setTimeout(() => {
+        authCtx.logout();
+      }, remainingTime)
+    );
+  };
   
   return (
     <header className={classes.header}>
